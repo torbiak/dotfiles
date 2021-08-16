b3b87ff4231ac53fd6f576536568f13753de2bad
Jordan Torbiak <torbiak@gmail.com>
2021-08-16 15:54:45 -0600

    Cleanup install.sh
    Seems my shellscript style has changed quite a bit since originally
    writing it.
    
    - add quotes around variable expansions where appropriate
    - add some comments, since it wasn't immediately obvious why rsync was
      being used.
    - add usage help

--- install.sh
+++ install.sh
@@ -1,95 +1,109 @@
 #!/usr/bin/env bash
 # install.sh
-# Installs symlinks in $HOME to point to the dotfiles in the repo.
+# Installs dotfiles to $HOME.
 # The dotfiles repo should be somewhere under $HOME.
 # If the repo directory is moved this script will need to be run again.
-# Old dotfiles are saved in a tarball in $HOME.
 set -eu
 
 # Get relative path from $HOME
-pushd $HOME 1>/dev/null
+pushd "$HOME" 1>/dev/null
 # Popd in a subshell, then popd in this shell to get back to the repo dir.
 absolute_repo_path=$(popd); popd &> /dev/null
 if [[ "$absolute_repo_path" != \~/* ]]; then
     echo 'dotfiles repo should be under $HOME' 1>&2
-    exit
+    exit 1
 fi
 home_to_repo_dir=${absolute_repo_path#\~/}
 
-# List of dotfiles tracked in the repo.
-dotfiles=$(
-    for path in dotfiles/*; do
-        f=$(basename $path)
-        echo ".$f"
-    done
-)
+# Array of dotfile basenames tracked in the repo.
+dotfiles=($(
+    cd dotfiles
+    ls -d * | sed 's#^#.#'
+))
 
-# List of tracked dotfiles that already exist in $HOME
-preexisting_dotfiles=$(
-    for file in $dotfiles; do
-        if [[ -e $HOME/$file ]]; then
-            echo $file
-        fi
+# Print tracked dotfile basenames that already exist in $HOME.
+preexisting_dotfiles() {
+    for file in "${dotfiles[@]}"; do
+        [[ -e "$HOME/$file" ]] || continue
+        echo "$file"
     done
-)
-
-# Convert dotfile filename to repo filepath.
-# eg: .vimrc -> dotfiles/vimrc
-function repo-filepath {
-    echo dotfiles/${file#.}
 }
 
 # Backup preexisting dotfiles
-function backup {
-  if [[ -n "$preexisting_dotfiles" ]]; then
-      backup="$HOME/dotfile_$(date +%FT%T).bak.tar.gz"
-      tar -C $HOME -czf $backup $preexisting_dotfiles
-  fi
+backup() {
+    local existing
+    existing=($(preexisting_dotfiles))
+    (("${#existing[@]}")) || return 0
+    backup="$HOME/dotfile_$(date +%FT%T).bak.tar.gz"
+    tar -C "$HOME" -czf "$backup" "${existing[@]}"
 }
 
-# Delete preexisting tracked dotfiles.
-function delete {
-  for file in $preexisting_dotfiles; do
-      rm -rf $HOME/$file
-  done
+delete_preexisting() {
+    local existing
+    existing=($(preexisting_dotfiles))
+    # Some rm implementations (eg BusyBox) fail when not given any args, so
+    # exit early if there's nothing to do.
+    (("${#existing[@]}")) || return 0
+    rm "${existing[@]/#/$HOME/}" || {
+        echo "\
+Couldn't delete all pre-existing dotfiles. If there are pre-existing dotdirs,
+please remove them manually, since automatically deleting dirs from a home
+directory is a dangerous operation to script.
+
+remaining pre-existing dotfiles:
+$(preexisting_dotfiles)
+" >&2
+        return 1
+    }
 }
 
 # Create symlinks ($HOME -> $DOTFILES_REPO)
-function symlink {
-    delete
-    for file in $dotfiles; do
-        ln -s "$home_to_repo_dir/dotfiles/${file#.}" $HOME/$file
+symlink() {
+    delete_preexisting
+    for file in "${dotfiles[@]}"; do
+        ln -s "$home_to_repo_dir/dotfiles/${file#.}" "$HOME/$file"
     done
 }
 
-function cp-to-home {
-    for file in $dotfiles; do
+# cp-to-home and cp-to-repo are intended for systems where symlinks are
+# unavailable or impractical. Instead of symlinking, the files are copied to
+# the home dir. To check what changes have been made to the installed dotfiles,
+# use cp-to-repo. rsync is used since there can be lots of files under dotdirs,
+# like plugins under .vim.
+cp-to-home() {
+    for file in "${dotfiles[@]}"; do
         local trailing=""
-        [[ -d $(repo-filepath $file) ]] && trailing='/'
-        rsync -r $(repo-filepath $file)$trailing $HOME/$file
+        [[ -d "${file#.}" ]] && trailing='/'
+        rsync -r "${file#.}$trailing" "$HOME/$file"
     done
 }
 
-function cp-to-repo {
+cp-to-repo() {
     if ! git branch > /dev/null; then
         echo "Problem detecting git repo."
         return 1
     fi
-    if [[ $(git status --porcelain) ]]; then
+    if [[ "$(git status --porcelain)" ]]; then
         echo "Repo has changes. Commit them before continuing."
         return 1
     fi
-    for file in $dotfiles; do
-        if [[ -L $HOME/$file ]]; then
+    for file in "${dotfiles[@]}"; do
+        if [[ -L "$HOME/$file" ]]; then
             echo "$file is a symlink. Do you really want to copy it to the repo?"
             return 1
         fi
-        [[ -e $HOME/$file ]] || continue
+        [[ -e "$HOME/$file" ]] || continue
         local trailing=""
-        [[ -d $HOME/$file ]] && trailing='/'
+        [[ -d "$HOME/$file" ]] && trailing='/'
         rsync -r $HOME/$file$trailing $(repo-filepath $file)
     done
 }
 
-cmd=${1:?No command given}
-$cmd
+usage="install.sh symlink|cp-to-home|cp-to-repo"
+while getopts 'h' opt; do
+    case "$opt" in
+    h) echo "$usage"; exit 0;;
+    esac
+done
+shift $((OPTIND-1))
+"$@"
