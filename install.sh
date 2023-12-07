77434cec7b36476f63497b053f933c4b3fb4eb84
Jordan Torbiak <torbiak@gmail.com>
2023-12-06 17:24:34 -0700

    Update install.sh and add cmus rc
    Update install.sh to be shellcheck-compliant. Add cmus rc, but since it
    gets installed at ~/.cmus/rc or $XDG_CONFIG_HOME/cmus/rc, it can't be
    installed like the rest of the dotfiles.

--- install.sh
+++ install.sh
@@ -1,75 +1,49 @@
-#!/usr/bin/env bash
-# install.sh
+#!/bin/bash
+
 # Installs dotfiles to $HOME.
 # The dotfiles repo should be somewhere under $HOME.
 # If the repo directory is moved this script will need to be run again.
-set -eu
 
-# Get relative path from $HOME
-pushd "$HOME" 1>/dev/null
-# Popd in a subshell, then popd in this shell to get back to the repo dir.
-absolute_repo_path=$(popd); popd &> /dev/null
-if [[ "$absolute_repo_path" != \~/* ]]; then
-    echo 'dotfiles repo should be under $HOME' 1>&2
-    exit 1
-fi
-home_to_repo_dir=${absolute_repo_path#\~/}
+set -euo pipefail
 
-# Array of dotfile basenames tracked in the repo.
-dotfiles=($(
-    cd dotfiles
-    ls -d * | sed 's#^#.#'
-))
-
-# Print tracked dotfile basenames that already exist in $HOME.
-preexisting_dotfiles() {
+# Create symlinks ($HOME -> $DOTFILES_REPO)
+symlink() {
+    delete_preexisting || return 1
     for file in "${dotfiles[@]}"; do
-        [[ -e "$HOME/$file" ]] || continue
-        echo "$file"
+        ln -s "$home_to_repo_dir/dotfiles/${file#.}" "$HOME/$file" || return 1
     done
+}
+
+delete_preexisting() {
+    # Some rm implementations (eg BusyBox) fail when not given any args, so
+    # exit early if there's nothing to do.
+    (("${#existing[@]}")) || return 0
+    local f rc
+    rc=0
+    for f in "${existing[@]}"; do
+        f=$HOME/$f
+        if [[ -d "$f" && ! -L "$f" ]]; then
+            # Removing dirs from a home directory is scary.
+            echo "Refusing to remove directory $f"
+            return 1
+        fi
+        rm "$f" || return 1
+    done
+    return "$rc"
 }
 
 # Backup preexisting dotfiles
 backup() {
-    local existing
-    existing=($(preexisting_dotfiles))
     (("${#existing[@]}")) || return 0
     backup="$HOME/dotfile_$(date +%FT%T).bak.tar.gz"
     tar -C "$HOME" -czf "$backup" "${existing[@]}"
 }
 
-delete_preexisting() {
-    local existing
-    existing=($(preexisting_dotfiles))
-    # Some rm implementations (eg BusyBox) fail when not given any args, so
-    # exit early if there's nothing to do.
-    (("${#existing[@]}")) || return 0
-    rm "${existing[@]/#/$HOME/}" || {
-        echo "\
-Couldn't delete all pre-existing dotfiles. If there are pre-existing dotdirs,
-please remove them manually, since automatically deleting dirs from a home
-directory is a dangerous operation to script.
-
-remaining pre-existing dotfiles:
-$(preexisting_dotfiles)
-" >&2
-        return 1
-    }
-}
-
-# Create symlinks ($HOME -> $DOTFILES_REPO)
-symlink() {
-    delete_preexisting
-    for file in "${dotfiles[@]}"; do
-        ln -s "$home_to_repo_dir/dotfiles/${file#.}" "$HOME/$file"
-    done
-}
-
 # cp-to-home and cp-to-repo are intended for systems where symlinks are
 # unavailable or impractical. Instead of symlinking, the files are copied to
 # the home dir. To check what changes have been made to the installed dotfiles,
-# use cp-to-repo. rsync is used since there can be lots of files under dotdirs,
-# like plugins under .vim.
+# use cp-to-repo and run `git diff`. rsync is used since there can be lots of
+# files under dotdirs, like plugins under .vim.
 cp-to-home() {
     for file in "${dotfiles[@]}"; do
         local trailing=""
@@ -95,14 +69,47 @@ cp-to-repo() {
         [[ -e "$HOME/$file" ]] || continue
         local trailing=""
         [[ -d "$HOME/$file" ]] && trailing='/'
-        rsync -r $HOME/$file$trailing "dotfiles/${file#.}"
+        rsync -r "$HOME/$file$trailing" "dotfiles/${file#.}"
     done
 }
 
-usage="install.sh symlink|cp-to-home|cp-to-repo"
+special() {
+    install_cmusrc ||
+    return 1
+}
+
+install_cmusrc() {
+    local cmus_config=~/.cmus
+    mkdir -p "$cmus_config" &&
+    ln -s "$abs_repo_path/dotfiles_special/cmusrc" "$cmus_config/rc"
+}
+
+
+abs_repo_path=$(cd "${BASH_SOURCE%/*}"; pwd)
+if [[ "$abs_repo_path" != $HOME* ]]; then
+    # shellcheck disable=SC2016
+    echo 'dotfiles repo should be under $HOME' 1>&2
+    exit 1
+fi
+home_to_repo_dir=${abs_repo_path#$HOME/}
+
+# Array of dotfile basenames tracked in the repo.
+dotfiles=()
+for f in dotfiles/*; do
+    dotfiles+=(."${f#*/}")
+done
+
+# dotfile basenames that already exist in $HOME.
+existing=()
+for file in "${dotfiles[@]}"; do
+    [[ -e "$HOME/$file" || -L "$HOME/$file" ]] && existing+=("$file")
+done
+
+usage="install.sh symlink|cp-to-home|cp-to-repo|special"
 while getopts 'h' opt; do
     case "$opt" in
     h) echo "$usage"; exit 0;;
+    *) exit 1;;
     esac
 done
 shift $((OPTIND-1))
